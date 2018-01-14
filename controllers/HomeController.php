1fcf9d045b5b125b5666ba9be8b4dc3f1f1d19be
HilkiahMakemo <hilkiah.makemo@skywest.com>
2018-01-14 05:47:21 -0700

    Update HomeController.php

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -5,7 +5,7 @@ namespace App\Site\Controllers;
 use FramePHP\Http\Request;
 use FramePHP\Http\Response;
 
-class HomeController extends Controller
+class HomeController extends SiteController
 {
 
   public function __invoke()
