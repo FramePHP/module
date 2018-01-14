48076bbe2c295f3390493edf00e404925a58f521
HilkiahMakemo <hilkiah.makemo@skywest.com>
2018-01-14 05:48:17 -0700

    Update and rename Model.php to BaseModel.php

---
+++ datamodels/BaseModel.php
@@ -0,0 +1,8 @@
+<?php
+
+use FramePHP\App\Data\Model;
+
+class BaseModel extends Model
+{
+
+}
--- datamodels/Model.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use FramePHP\App\Data\Model as BaseModel;
-
-class Model extends BaseModel
-{
-
-}
