72a636af317ddd51575b2c99f3c603dd957c64d0
HilkiahMakemo <hilkiah.makemo@skywest.com>
2018-01-14 08:39:04 -0700

    Update index.php

--- templates/content/index.php
+++ templates/content/index.php
@@ -1,1 +1,15 @@
-{{ "Twig<br />" }}
+{% extends layouts('main')" %}
+
+{% block title %}Index{% endblock %}
+{% block head %}
+    {{ parent() }}
+    <style type="text/css">
+        .important { color: #336699; }
+    </style>
+{% endblock %}
+{% block content %}
+    <h1>Index</h1>
+    <p class="important">
+        Welcome to my awesome homepage.
+    </p>
+{% endblock %}
